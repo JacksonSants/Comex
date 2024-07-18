3f07c2c5802e00a181ac48762fd68401a47e1c00
JacksonSants <jacksondiego2002@gmail.com>
2024-07-18 09:58:26 -0400

    Criacao da classe Produto

---
+++ Comex/Produto.cs
@@ -0,0 +1,7 @@
+﻿class Produto
+{
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public float PrecoUnitario { get; set; }
+    public int Quantidade { get; set; }
+}
