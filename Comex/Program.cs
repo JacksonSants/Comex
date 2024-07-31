e58a23ace4d94736f6342cc8a13541889f417f32
Jackson Ribeiro <jacksonribeiro@bemol.com.br>
2024-07-30 21:19:03 -0400

    inserção das classes Produto e Endereco e uso de Construtor para criar um Produto.

--- Comex/Program.cs
+++ Comex/Program.cs
@@ -1,92 +1,103 @@
-﻿internal class Program
+﻿//card welcome
+using System.Globalization;
+
+string welcome = "Welcome to Comex";
+List<Produto> produtos = new List<Produto>();
+//card meu
+void ExibirLogo()
 {
-    private static void Main(string[] args)
-    {
-        //card welcome
-        string welcome = "Welcome to Comex";
-        List<Produto> produtos = new List<Produto>();
-        //card meu
-        void ExibirLogo()
-        {
-            Console.WriteLine(@"
+    Console.WriteLine(@"
         ░█████╗░░█████╗░███╗░░░███╗███████╗██╗░░██╗
         ██╔══██╗██╔══██╗████╗░████║██╔════╝╚██╗██╔╝
         ██║░░╚═╝██║░░██║██╔████╔██║█████╗░░░╚███╔╝░
         ██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░░██╔██╗░
         ╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗██╔╝╚██╗
           ░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝");
-            Console.WriteLine(welcome);
-        }
-
-        void CriarProduto()
-        {
-            Console.Clear();
-            Console.WriteLine("----Registro de produto----");
-            Console.Write("Digite o nome do produto: ");
-            string newProduto = Console.ReadLine()!;
-            Produto produto = new Produto(newProduto);
-            produtos.Add(produto);
-            Console.WriteLine($"\nProduto {produto.Nome} adicionado com sucesso");
-            Console.WriteLine("Aperter enter para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
-            ExibirOpcoesMenu();
-
-
-        }
-
-        void ListaProduto()
-        {
-            Console.Clear();
-            Console.WriteLine("----Lista de Produtos----\n");
-            if (produtos.Count > 0)
-            {
-                foreach (Produto produto in produtos)
-                {
-                    Console.WriteLine($"Produto: {produto.Nome}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Nenhum produto registrado.");
-            }
-            Console.WriteLine("\nAperte enter para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
-            ExibirOpcoesMenu();
-
-        }
-
-        void ExibirOpcoesMenu()
-        {
-            ExibirLogo();
-            Console.WriteLine("\n1 - Criar produto\n2 - Listar produto\n0 - Sair");
-            Console.Write("Opção: ");
-            string opcao = Console.ReadLine()!;
-            int opcaoEscolhida = int.Parse(opcao);
-
-            switch (opcaoEscolhida)
-            {
-                case 1:
-                    // Create product
-                    Produto.CriarProduto(produtos);
-                    break;
-                case 2:
-                    // List products
-                    Produto.ListaProduto(produtos);
-                    break;
-                case 0:
-                    // Exit
-                    Console.WriteLine("Obrigado(a) pela preferência, volte sempre.");
-                    return;
-                default:
-                    Console.WriteLine("Opção inválida");
-                    break;
-            }
-            Console.Clear();
-            ExibirOpcoesMenu();
-        }
-
-        ExibirOpcoesMenu();
-    }
+    Console.WriteLine(welcome);
 }
+
+void CriarProduto()
+{
+    Console.Clear();
+    Console.WriteLine("----Registro de produto----");
+    Console.Write("Digite o nome do produto: ");
+    string nomeProduto = Console.ReadLine()!;
+
+    Console.Write("Descrição do produto: ");
+    string descricao = Console.ReadLine();
+
+    Console.Write("Preço: ");
+    string precoInput = Console.ReadLine();
+    float precoUnitario = float.Parse(precoInput, CultureInfo.InvariantCulture);
+
+    Console.Write("Quantidade: ");
+    int quantidade = int.Parse(Console.ReadLine());
+
+    Produto produto = new Produto(nomeProduto, descricao, precoUnitario, quantidade);
+    produtos.Add(produto);
+
+    Console.WriteLine($"\nProduto {produto.Nome} adicionado com sucesso");
+    Console.WriteLine("Aperter enter para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesMenu();
+
+
+}
+
+void ListaProduto()
+{
+    Console.Clear();
+    Console.WriteLine("----Lista de Produtos----\n");
+    if (produtos.Count > 0)
+    {
+        foreach (Produto produto in produtos)
+        {
+            Console.WriteLine($"Produto: {produto.Nome}");
+            Console.WriteLine($"Produto: {produto.Descricao}");
+            Console.WriteLine($"Produto: {produto.PrecoUnitario}");
+            Console.WriteLine($"Produto: {produto.Quantidade}\n");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Nenhum produto registrado.");
+    }
+    Console.WriteLine("\nAperte enter para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesMenu();
+
+}
+
+void ExibirOpcoesMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\n1 - Criar produto\n2 - Listar produto\n0 - Sair");
+    Console.Write("Opção: ");
+    string opcao = Console.ReadLine()!;
+    int opcaoEscolhida = int.Parse(opcao);
+
+    switch (opcaoEscolhida)
+    {
+        case 1:
+            // Create product
+            CriarProduto();
+            break;
+        case 2:
+            // List products
+            ListaProduto();
+            break;
+        case 0:
+            // Exit
+            Console.WriteLine("Obrigado(a) pela preferência, volte sempre.");
+            return;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+    Console.Clear();
+    ExibirOpcoesMenu();
+}
+
+ExibirOpcoesMenu();
